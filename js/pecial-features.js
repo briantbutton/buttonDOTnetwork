1015da5da80db3196f274a19e242a855e97d1c34
Brian Button <bb@pegas.us.com>
2021-07-13 16:11:44 -0700

    Responsive design for some text; moved js out of index.html

--- js/pecial-features.js
+++ js/pecial-features.js
@@ -1,27 +1,32 @@
       (function(){
 
-        const   k                     = [["y","top","Y"],["x","left","X"],["top","top","Y"],["left","left","X"],["bottom","bottom","Y"],["right","right","X"],["height","height"],["width","width"]];
+        const k                       = [["y","top","Y"],["x","left","X"],["top","top","Y"],["left","left","X"],["bottom","bottom","Y"],["right","right","X"],["height","height"],["width","width"]];
+        var   vid1                    = document.getElementById("colored-lights"),
+              vid2                    = document.getElementById("two-minute-tale"),
+              upcell                  = document.getElementById("upsell"),
+              noUpsell                = false;
 
         window.InView                 = (function InView(){
 
           return InView;
 
-          function InView(idStr,act,lpse){
-            var target                = document.getElementById(idStr),
-                action                = act!=="play"?"play":act,
+          function InView(idStr){
+            var video                 = document.getElementById(idStr),
                 lapse                 = typeof lpse === "number" ? lpse : 60000,
                 lastPlayed            = (new Date()).getTime()-lapse-1,
                 isOffScreen           = true;
-            if(target){$(window).scroll(check)}
+            if(video){
+              $(window).scroll(check)
+            }
 
             function check(){
               var now                 = (new Date()).getTime();
-              if(objInView(target)){
+              if(objInView(video)){
                 if(isOffScreen){
                   isOffScreen         = false;
                   if(now-lastPlayed>lapse){
                     lastPlayed        = now;
-                    target[action]()
+                    video.play()
                   }
                 }
               }else{
@@ -100,60 +105,98 @@
               }
             }
             function contractVideoB(){
-              oppoC1.style.opacity  = "1.00";
-              oppoC1.className      = "entry col-md-4";
-              oppoC2.style.opacity  = "1.00";
-              oppoC2.className      = "entry col-md-4";
+              oppoC1.style.opacity    = "1.00";
+              oppoC1.className        = "entry col-md-4";
+              oppoC2.style.opacity    = "1.00";
+              oppoC2.className        = "entry col-md-4";
               if(oppoV1){
-                oppoV1.style.width  = ""+initWidth+"px";
-                oppoV1.style.height = ""+initHeight+"px";
+                oppoV1.style.width    = ""+initWidth+"px";
+                oppoV1.style.height   = ""+initHeight+"px";
               }
               if(oppoV2){
-                oppoV2.style.width  = ""+initWidth+"px";
-                oppoV2.style.height = ""+initHeight+"px";
+                oppoV2.style.width    = ""+initWidth+"px";
+                oppoV2.style.height   = ""+initHeight+"px";
               }
-              container.className   = "entry col-md-4"
+              container.className     = "entry col-md-4"
             }
             function calibrate(){
-              containerWidth        = 720;
-              initWidth             = 208;              // off 210
-              initHeight            = 117;
-              compressedWidth       =  80;              // off  90
-              compressedHeight      =  45;
-              playWidth             = 448;              // off 450
-              playHeight            = 252;
+              containerWidth          = 720;
+              initWidth               = 208;              // off 210
+              initHeight              = 117;
+              compressedWidth         =  80;              // off  90
+              compressedHeight        =  45;
+              playWidth               = 448;              // off 450
+              playHeight              = 252;
               if(window.innerWidth>992){
                 if(window.innerWidth>1200){
-                  containerWidth    = 1140;
-                  initWidth         = 336;              // off 350
-                  initHeight        = 189;
-                  compressedWidth   = 160;              // off 160
-                  compressedHeight  =  90;
-                  playWidth         = 720;              // off 720
-                  playHeight        = 405
+                  containerWidth      = 1140;
+                  initWidth           = 336;              // off 350
+                  initHeight          = 189;
+                  compressedWidth     = 160;              // off 160
+                  compressedHeight    =  90;
+                  playWidth           = 720;              // off 720
+                  playHeight          = 405
                 }else{
-                  containerWidth    = 960;
-                  initWidth         = 288;              // off 290
-                  initHeight        = 162;
-                  compressedWidth   = 128;              // off 130
-                  compressedHeight  =  72;
-                  playWidth         = 640;              // off 640
-                  playHeight        = 360
+                  containerWidth      = 960;
+                  initWidth           = 288;              // off 290
+                  initHeight          = 162;
+                  compressedWidth     = 128;              // off 130
+                  compressedHeight    =  72;
+                  playWidth           = 640;              // off 640
+                  playHeight          = 360
                 }
               }
             }
           }
         })();
 
-        videoPlayer0                = new InView("colored-lights");
-        videoExpander0              = new Rejigger3Columns("piglets",0);
-        videoExpander1              = new Rejigger3Columns("piglets",1);
+
+
+        window.videoPlayer0           = new InView("colored-lights");
+        window.videoExpander0         = new Rejigger3Columns("piglets",0);
+        window.videoExpander1         = new Rejigger3Columns("piglets",1);
+        window.videoExpander1         = new Rejigger3Columns("piglets",2);
+        // videobaiter                 = new Baiter("colored-lights","two-minute-tale","upsell",["upsell1","upsell2"]);
+
+        if(vid1&&vid2&&upcell){
+          vid1.addEventListener("click",switchBait);
+          vid1.addEventListener("play",turnOffUpsell);
+          vid2.addEventListener("play",turnOffUpsellForever);
+          vid1.addEventListener("ended",turnOnUpsell);
+          setTimeout((e1=>()=>backToTop(e1))(document.getElementById("top-line-text1")),4000);
+          setTimeout((e2=>()=>backToTop(e2))(document.getElementById("top-line-text2")),8000);
+        }
+
+        function switchBait(){
+          vid1.style.display          = "none";
+          vid2.style.display          = "inline";
+          vid2.play()
+        }
+        function backToTop(e){
+          if(e){
+            e.style.opacity           = "1";
+            e.style.marginTop         = "0px"
+          }
+        }
+        function turnOffUpsellForever(){
+          upcell.style.display        = "none";
+          noUpsell                    = true
+        }
+        function turnOffUpsell(){
+          upcell.style.display        = "none"
+        }
+        function turnOnUpsell(){
+          if(!noUpsell){
+            upcell.style.display      = "block"
+          }
+        }
+
 
         function getCoord(HTMLobj,noWinOffset_){
-          var noWinOffset           = true,
-              v                     = null,r;
+          var noWinOffset             = true,
+              v                       = null,r;
           if(Boolean(HTMLobj)&&Boolean(r=HTMLobj.getBoundingClientRect())){
-            v                       = {};
+            v                         = {};
             k.forEach(function(d){
               if ( typeof r[d[0]] === "number" ) {
                 v[d[1]]=Math.floor(r[d[0]]+(Boolean(d[2])&&!noWinOffset?window["page"+d[2]+"Offset"]:0)+0.5)
